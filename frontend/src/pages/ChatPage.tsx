c58f5685429565904fe7e3f8fca2f7e8d9e9df05
Zelalem Tefera <zedoyetefe@gmail.com>
2025-09-29 18:24:13 +0300

    create chat page

---
+++ frontend/src/pages/ChatPage.tsx
@@ -0,0 +1,11 @@
+function ChatPage(){
+    return(
+        <div>
+            <h1>here is chat page </h1>
+            <h2> under mainatainance
+            </h2>
+        </div>
+    )
+}
+
+export default ChatPage
